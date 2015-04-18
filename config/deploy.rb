9a6bc0db956d2f0b27832f381f485c1d3e46c64b
Tianyu Huang <tianhsky@gmail.com>
2015-04-18 01:09:04 -0400

    config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,11 @@ set :log_level, :info
 set :keep_releases, 10
 set :linked_dirs, %w{log tmp}
 
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
 namespace :deploy do
 
   after :publishing, :bundle_install do
