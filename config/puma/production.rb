62914cc3f169ded72d034ded830f7611b1e57a57
Tianyu Huang <tianhsky@gmail.com>
2015-04-18 19:31:09 -0400

    test

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,15 +1,15 @@
 env = 'production'
-app_path = "/home/deployer/srv/#{env}/lolcaf"
+app_path = File.expand_path("../../", Dir.pwd)
 
 environment env
 daemonize false
 workers 5
 threads 0, 20
+prune_bundler
 
 bind  "unix://#{app_path}/shared/tmp/sockets/puma.sock"
 pidfile "tmp/pids/puma.pid"
 state_path "tmp/pids/puma.state"
 stdout_redirect "log/#{env}.out.log", "log/#{env}.err.log", true
 
-# prune_bundler
 # activate_control_app
