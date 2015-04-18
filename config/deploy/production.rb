15ba4c7df7c87405c0d147eddc89b25acdf23ea5
Tianyu Huang <tianhsky@gmail.com>
2015-04-18 00:17:00 -0400

    config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server 'www.lolcaf.com', roles: %w{app}, user: 'root'
 
 set :rails_env, 'production'
 set :branch, 'master'
-set :deploy_to, "/srv/production/lolcaf"
+set :deploy_to, "/home/deployer/srv/production/lolcaf"
