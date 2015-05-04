c177e634bc60cd6097b8b16d5239144b458d0cc2
Tianyu Huang <tianhsky@gmail.com>
2015-05-04 01:12:53 -0400

    phased restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,9 @@ namespace :deploy do
     on roles(:app), in: :groups, limit: 3, wait: 10 do
       within shared_path do
         execute :eye, "load eye/app*.rb"
-        execute :eye, "restart lolcaf_app*"
+        execute :eye, "restart lolcaf_app"
+        sleep 20
+        execute :eye, "restart lolcaf_app2"
       end
     end
   end
