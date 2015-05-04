32e7d870313e163915e8c0ea9058cd1d0c4d96b4
Tianyu Huang <tianhsky@gmail.com>
2015-05-04 01:08:18 -0400

    deploy config change

--- config/deploy/production2.rb
+++
@@ -1,6 +0,0 @@
-server 'app1.nj.lolcaf.com:2223', roles: %w{app}, user: 'deployer'
-
-set :app_name, 'lolcaf2'
-set :rails_env, 'production'
-set :branch, 'master'
-set :deploy_to, "/home/deployer/srv/production/lolcaf2"
