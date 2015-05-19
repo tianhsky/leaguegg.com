9a03bb9aad2e07e88b6c701a2d99512afb87135d
Tianyu Huang <tianhsky@gmail.com>
2015-05-19 01:25:42 -0400

    static cache expire condition

--- app/models/consts/static_data.rb
+++ app/models/consts/static_data.rb
@@ -3,8 +3,7 @@ module Consts
   class StaticData
 
     def self.local_cache_expired?
-      not_expired = @data && @updated_at && @updated_at >= Time.now - AppConsts::RIOT_CONSTS_EXPIRES_THRESHOLD
-      !not_expired
+      !@data || @updated_at && @updated_at < Time.now - AppConsts::RIOT_CONSTS_EXPIRES_THRESHOLD
     end
 
   end
