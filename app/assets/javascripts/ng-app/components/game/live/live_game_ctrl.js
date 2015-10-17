cf58386505e9f789517876e7ffc0b707c4903bc7
Tianyu Huang <tianhsky@gmail.com>
2015-10-17 11:58:46 -0400

    history stats

--- app/assets/javascripts/ng-app/components/game/live/live_game_ctrl.js
+++ app/assets/javascripts/ng-app/components/game/live/live_game_ctrl.js
@@ -23,15 +23,19 @@ angular.module('leaguegg.game').controller('LiveGameCtrl', [
     LiveGameService.getGameBySummoner($scope.query)
       .then(function(resp) {
         $scope.loading.game.active = false;
+        $scope.error = null;
+        $scope.error_summoner_not_in_game = false;
         if (resp.data && resp.data.id == null) {
           if (resp.status == 404) {
             $scope.error = resp.data.error;
+            if ($scope.error == 'Summoner is not currently in game') {
+              $scope.error_summoner_not_in_game = true;
+            }
           } else {
             $scope.error = "Sorry, there was a problem";
           }
         } else {
-          $scope.error = null;
-          LiveGameService.applyOPGGUrl(resp);
+          // LiveGameService.applyOPGGUrl(resp);
           $scope.game = resp;
         }
       });
