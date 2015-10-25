0937b6a2ddad0fb1b28e3363c8fdb4f5ba05d3a1
Tianyu Huang <tianhsky@gmail.com>
2015-10-25 11:47:03 -0400

    feedback style

--- vendor/assets/bower_components/angular-send-feedback/dist/angular-send-feedback.js
+++ vendor/assets/bower_components/angular-send-feedback/dist/angular-send-feedback.js
@@ -69,7 +69,7 @@ angular.module('angular-send-feedback').directive('angularFeedback', [ function(
                         var _html2canvas = false;
                         if (supportedBrowser) {
                             if(isFeedbackButtonNative) {
-                                $('body').append('<button class="feedback-btn feedback-btn-gray">' + settings.initButtonText + '</button>');
+                                $('body').append('<button class="feedback-btn btn btn-sm btn-default"><i class="fa fa-comment"></i> ' + settings.initButtonText + '</button>');
                             }
                             $(document).on('click', settings.feedbackButton, function(){
                                 if(isFeedbackButtonNative) {
