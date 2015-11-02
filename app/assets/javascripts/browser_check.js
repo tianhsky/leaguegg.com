53ab5d3c9a17ca838274918ba942058ce99c3e9d
Tianyu Huang <tianhsky@gmail.com>
2015-11-02 00:44:57 -0500

    browser check

--- app/assets/javascripts/browser_check.js
+++ app/assets/javascripts/browser_check.js
@@ -6,7 +6,7 @@ $(function() {
       if (version <= 9) {
         $('#page').empty();
         $('#error-browser-not-support').show();
-        location.href = "/browser_error";
+        location.href = "/browser_error.html";
       }
     }
   }
