a80e6431dd0ff52c0dc9b95202f0cc0aeaae7bfd
Tianyu Huang <tianhsky@gmail.com>
2017-03-13 00:36:36 -0400

    update api endpoint

--- app/services/static_data_service/riot.rb
+++ app/services/static_data_service/riot.rb
@@ -4,37 +4,37 @@ module StaticDataService
 
     def self.fetch_champions(region='na')
       # url = "https://global.api.pvp.net/api/lol/static-data/#{region.downcase}/v1.2/champion"
-      url = "http://ddragon.leagueoflegends.com/cdn/5.24.1/data/en_US/champion.json"
+      url = "http://ddragon.leagueoflegends.com/cdn/7.5.2/data/en_US/champion.json"
       resp = RiotAPI.get(url, region)
     end
 
     def self.fetch_maps(region='na')
       # url = "https://global.api.pvp.net/api/lol/static-data/#{region.downcase}/v1.2/map"
-      url = "http://ddragon.leagueoflegends.com/cdn/5.24.1/data/en_US/map.json"
+      url = "http://ddragon.leagueoflegends.com/cdn/7.5.2/data/en_US/map.json"
       resp = RiotAPI.get(url, region)
     end
 
     def self.fetch_masteries(region='na')
       # url = "https://global.api.pvp.net/api/lol/static-data/#{region.downcase}/v1.2/mastery?masteryListData=all"
-      url = "http://ddragon.leagueoflegends.com/cdn/5.24.1/data/en_US/mastery.json"
+      url = "http://ddragon.leagueoflegends.com/cdn/7.5.2/data/en_US/mastery.json"
       resp = RiotAPI.get(url, region)
     end
 
     def self.fetch_runes(region='na')
       # url = "https://global.api.pvp.net/api/lol/static-data/#{region.downcase}/v1.2/rune?runeListData=all"
-      url = "http://ddragon.leagueoflegends.com/cdn/5.24.1/data/en_US/rune.json"
+      url = "http://ddragon.leagueoflegends.com/cdn/7.5.2/data/en_US/rune.json"
       resp = RiotAPI.get(url, region)
     end
 
     def self.fetch_items(region='na')
       # url = "https://global.api.pvp.net/api/lol/static-data/#{region.downcase}/v1.2/item"
-      url = "http://ddragon.leagueoflegends.com/cdn/5.24.1/data/en_US/item.json"
+      url = "http://ddragon.leagueoflegends.com/cdn/7.5.2/data/en_US/item.json"
       resp = RiotAPI.get(url, region)
     end
 
     def self.fetch_spells(region='na')
       # url = "https://global.api.pvp.net/api/lol/static-data/#{region.downcase}/v1.2/summoner-spell"
-      url = "http://ddragon.leagueoflegends.com/cdn/5.24.1/data/en_US/summoner.json"
+      url = "http://ddragon.leagueoflegends.com/cdn/7.5.2/data/en_US/summoner.json"
       resp = RiotAPI.get(url, region)
     end
 
